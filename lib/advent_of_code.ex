11b9fe7641bc057a763c221b8715bae8df3aee9c
sulealtintas <sule@live.dk>
2022-12-01 22:50:50 +0100

    Refactor, add helper

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,18 +1,8 @@
 defmodule AdventOfCode do
-  @moduledoc """
-  Documentation for `AdventOfCode`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AdventOfCode.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec read_input(integer) :: binary
+  def read_input(day) do
+    day_padded = String.pad_leading(Integer.to_string(day), 2, "0")
+    {:ok, input} = File.read('./lib/advent_of_code/day#{day_padded}/input.txt')
+    input
   end
 end
--- lib/advent_of_code/day01/day01.ex
+++ lib/advent_of_code/day01/day01.ex
@@ -1,8 +1,7 @@
 defmodule AdventOfCode.Day01 do
   @spec input :: binary
   def input do
-    {:ok, input} = File.read('./lib/advent_of_code/day01/input.txt')
-    input
+    AdventOfCode.read_input(1)
   end
 
   @spec solution :: %{puzzle1: number, puzzle2: number}
@@ -13,32 +12,24 @@ defmodule AdventOfCode.Day01 do
   @spec puzzle1(binary) :: number
   def puzzle1(input) do
     input
-    |> calorie_lists_from_input()
-    |> calories_per_elf()
-    |> Enum.sort(:desc)
+    |> calories_per_elf_sorted()
     |> Enum.at(0)
   end
 
   @spec puzzle2(binary) :: number
   def puzzle2(input) do
     input
-    |> calorie_lists_from_input()
-    |> calories_per_elf()
-    |> Enum.sort(:desc)
+    |> calories_per_elf_sorted()
     |> Enum.slice(0..2)
     |> Enum.sum()
   end
 
-  @spec calorie_lists_from_input(binary) :: list
-  def calorie_lists_from_input(input) do
+  defp calories_per_elf_sorted(input) do
     input
     |> String.split("\n\n")
     |> Enum.map(&String.split(&1, "\n", trim: true))
     |> Enum.map(fn (arr) -> Enum.map(arr, &String.to_integer(&1)) end)
-  end
-
-  @spec calories_per_elf(any) :: list
-  def calories_per_elf(calorie_lists) do
-    Enum.map(calorie_lists, &Enum.sum(&1))
+    |> Enum.map(&Enum.sum(&1))
+    |> Enum.sort(:desc)
   end
 end
