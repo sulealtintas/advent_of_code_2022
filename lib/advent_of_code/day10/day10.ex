b220e9f4f9fe21556143082ce15385427f6b9aa7
sulealtintas <sule@live.dk>
2022-12-12 13:54:25 +0100

    Refactor day 10

--- lib/advent_of_code/day10/day10.ex
+++ lib/advent_of_code/day10/day10.ex
@@ -12,58 +12,46 @@ defmodule AdventOfCode.Day10 do
   @spec puzzle1(binary) :: non_neg_integer
   def puzzle1(input) do
     input
-    |> parse()
     |> value_by_cycle()
-    |> Enum.map(&interesting_signals/1)
+    |> Enum.drop(19)
+    |> Enum.take_every(40)
+    |> Enum.map(fn {cycle, value} -> cycle * value end)
     |> Enum.sum()
   end
 
   @spec puzzle2(binary) :: binary
   def puzzle2(input) do
     input
-    |> parse()
     |> value_by_cycle()
     |> Enum.chunk_every(40)
+    |> Enum.take(6)
     |> Enum.map_join("\n", &draw/1)
   end
 
-  defp parse(input) do
+  defp value_by_cycle(input) do
     input
     |> String.split("\n", trim: true)
-    |> Stream.map(&String.split(&1, " "))
+    |> Enum.flat_map_reduce([{1, 1}], fn cmd, acc ->
+      result = operate(cmd, List.last(acc))
+      {operate(cmd, List.last(acc)), acc ++ result}
+    end)
+    |> elem(1)
   end
 
   defp operate(command, {cycle, value}) do
     case command do
-      ["noop"] -> [{cycle + 1, value}]
-      [_, v] -> [{cycle + 1, value}, {cycle + 2, value + String.to_integer(v)}]
+      "noop" ->
+        [{cycle + 1, value}]
+
+      "addx " <> v ->
+        [{cycle + 1, value}, {cycle + 2, value + String.to_integer(v)}]
     end
-  end
-
-  defp value_by_cycle(commands, init \\ {1, 1}) do
-    Enum.reduce(commands, [init], fn cmd, acc ->
-      acc ++ operate(cmd, List.last(acc))
-    end)
-    |> Enum.reverse()
-    |> tl()
-    |> Enum.reverse()
-  end
-
-  defp interesting_signals({cycle, value}) do
-    cond do
-      cycle == 20 || rem(cycle + 20, 40) == 0 -> cycle * value
-      true -> 0
-    end
-  end
-
-  defp sprite_position(value) do
-    (value - 1)..(value + 1)
   end
 
   defp draw(row) do
     Enum.map_join(row, fn {cycle, value} ->
       cond do
-        rem(cycle - 1, 40) in sprite_position(value) -> "#"
+        rem(cycle - 1, 40) in (value - 1)..(value + 1) -> "#"
         true -> "."
       end
     end)
